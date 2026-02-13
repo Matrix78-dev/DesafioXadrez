13cd91112a22def0c2df77cab9edf1a1c2d36d57
Davi mota <davi.mota68@outlook.com>
2026-02-13 00:36:55 +0000

    desafioXadrez Completo

--- Desafio_Novato/Xadrez_Novato.c
+++ Desafio_Novato/Xadrez_Novato.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 
+//Aplicação recursivo de decisão para movimento da torre.
+void recursivoTorre (int numero1)
+{
+    if (numero1 > 0) {
+        printf ("Direita \n");
+        recursivoTorre (numero1 - 1); 
+    }
+}
+
+void recursivoRainha (int numero2)
+{
+    if (numero2 > 0) {
+        printf ("Esquerda\n");
+        recursivoRainha (numero2 - 1);
+    }
+}
+void recursivoBispo(int linha, int coluna) {
+
+    if (linha < 0 || coluna > 5) {
+        return;
+    }
+
+    printf("Cima\n");
+    printf("Direita\n");
+
+    recursivoBispo(linha - 1, coluna + 1);
+}
+
+
 int main () {
     int i = 1, option;
-    int movimentoCavalo;
+    int movimentoCavalo, contagemTorre = 5;
 
     do {
         printf ("Selecione uma opçao para movimentar uma peça de xadrez.\n");
         printf ("\n1. Torre move 5 casas para direita.\n");
-        printf ("2. Bispo move 5 casa diagonal cima, direita.\n");
+        printf ("2. Bispo move 6 casas na diagonal para cima e direita.\n");
         printf ("3. Rainha move 4 casas para esquerda.\n");
         printf ("4. Cavalo move duas casas para cima e uma para direita.\n");
         printf ("5. Sair.\n");
@@ -22,38 +51,39 @@ int main () {
         {
         case 1:
             printf(".:: Movimentação Torre ::.\n");
-            for (i = 1; i <= 6; i++) {
-                printf ("Direita\n");
-            }
+            recursivoTorre (contagemTorre);
+            printf ("\n");
             printf ("Torre moveu 5 casas para Direita.\n");
             break;
         case 2:
-            i = 1;
             printf(".:: Movimentação Bispo ::.\n");
-            while (i <= 5)
-            {
-                printf ("Cima, direita.\n");
-                i++;
-            }
-            printf ("Bispo moveu 5 casas na diagonal cima para direita.\n");
+            recursivoBispo(4, 3);
+            printf ("\n");
+            printf ("O bispo moveu 6 casas na diagonal para cima e para direita.\n");
             break;
         case 3:
-            i = 1;
             printf(".:: Movimentação Rainha ::.\n");
-            do { 
-                printf ("Esquerda.\n");
-                i++;               
-            } while (i <= 4);
+            int contagemRainha = 4;
+            recursivoRainha (contagemRainha);
+            printf ("\n");
             printf ("Rainha moveu 4 casas para esuqerda.\n");
             break;
         case 4:
-            for (movimentoCavalo = 0; movimentoCavalo < 1; movimentoCavalo++)
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    printf ("Cima\n");
-                }               
-                printf ("Direita\n");
+            // movimentação do cavalo com loop for +continue; +break;
+            for (int i = 0; i < 3; i++) {
+
+                if (i == 3) {
+                break; 
+                }
+
+                if (i < 2) {
+                printf("Cima\n");
+                continue; 
+                }
+
+                if (i == 2) {
+                printf("Direita\n");
+                }
             }
             printf ("O cavalo moveu duas casas para cima e uma para a direita.\n");            
             break;
@@ -69,3 +99,41 @@ int main () {
 
     return 0;
 }
+
+
+
+
+
+
+
+/*
+
+            for (i = 1; i <= 6; i++) {
+                printf ("Direita\n");
+            }
+            
+
+i = 1;
+            
+            do { 
+                printf ("Esquerda.\n");
+                i++;               
+            } while (i <= 4);
+            
+for (movimentoCavalo = 0; movimentoCavalo < 1; movimentoCavalo++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    printf ("Cima\n");
+                }               
+                printf ("Direita\n");
+  
+                }
+i = 1;
+            
+            while (i <= 5)
+            {
+                printf ("Cima, direita.\n");
+                i++;
+            }
+*/
